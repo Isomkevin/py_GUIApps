4223d6d8c2afcd62a1c10da3ce221c29a90b2e04
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2024-03-01 18:46:07 +0300

    Social Media Bookmark App tutorial finished

--- Social Media Bookmark/main_app.py
+++ Social Media Bookmark/main_app.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 import webbrowser
 
-linkedin_profile = "https://www.linkedin.com/in/kevin-isom-a58bb3201/"
-facebook_profile = "https://www.facebook.com/profile.php?id=100015443014495"
 
 def linkedin(event):
     webbrowser.open_new_tab(' put your linkedin profile link here ')
@@ -18,9 +16,9 @@ window.geometry("300x200")
 label1=tk.Label(text="My Social media", font=("Times new roman", 16))
 label1.grid(column=0,row=0)
 
-button1=tk.Button(window,text="Linkedin",bg="orange")
+button1=tk.Button(window, text="Linkedin", bg="orange")
 button1.grid(column=1,row=1)
-button2=tk.Button(window,text="Facebook",bg="pink")
+button2=tk.Button(window, text="Facebook", bg="pink")
 button2.grid(column=3,row=1)
 
 button1.bind("<Button-1>",linkedin)
